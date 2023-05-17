1fe832061401f4924cad635ec2970fb6d4e99f8c
Joel <valenciajoel0@gmail.com>
2023-05-16 22:29:19 -0300

    Se añadio el .js que contiene la tarea de realizar la conversacion entre usuarios

---
+++ conversation.js
@@ -0,0 +1,18 @@
+//Conversación entre dos usuarios
+
+//Un usuario puede iniciar una conversación con otro
+// Busco al usuario con quien voy a conversar
+//const other = users.find('email@example.com')
+// Inicio una conversación especificando el usuario
+//const conversation = new Conversation(other)
+
+//Si hacemos un console.log la conversation debe tener lo siguiente:
+/*
+{
+"initiator_id" : "12",
+"receiver_id" : "1234",
+"blocked" : false,
+"blocked_by"": null,
+}
+*/
+
