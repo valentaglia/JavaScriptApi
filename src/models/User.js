f83ecdd116795f541a2fbb9872f4d323335c0788
Joel <valenciajoel0@gmail.com>
2023-05-16 23:05:29 -0300

    Se añadio funciones y atributos para poder realizar la tarea. Se creo el atributo Contacts. Se implemento el metodo findByEmail

--- src/models/User.js
+++ src/models/User.js
@@ -1,7 +1,12 @@
+const ContactRepository = require("../repositories/ContactsRepository");
+
 class User {
     #id
     #email
     #password
+    //Se agrego un nuevo atributo a la clase User llamado contacts
+    //el cual es un JSON que contiene todos los contactos del usuario
+    #contacts = new ContactRepository
 
     setId(id) {
         this.#id = id
@@ -30,6 +35,17 @@ class User {
     isSamePassword(pwd) {
         return pwd === this.#password
     }
+    //Seteo el url de donde se encuentran los contactos del usuario
+    //para luego poder ir a buscarlos
+    setContacts(){
+        this.#contacts = new ContactRepository
+    }
+    //Busco en mi json de contactos al usuario con el email identico al parametro
+    findContact(email){
+        let  user = this.#contacts.findByEmail(email);
+        return user;
+    }
+
 }
 
 module.exports = User
