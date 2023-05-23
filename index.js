9be5b4230d1774e2b76d52d9f340559486f33882
sebmartinezpedemonte <sebastianmartinezpedemonte@gmail.com>
2023-05-23 19:37:42 -0300

    se agrega el archivo index modificado para probar la implementacion de createProduct

--- index.js
+++ index.js
@@ -5,6 +5,27 @@ const App = require('./src/App')
 const app = new App
 
 try {
+    const result = app.createProduct(
+        "Iphone XXI",
+        5600.99,
+        "USD",
+        10
+    )
+
+    console.log(result.getCurrency())
+} catch (e) {
+    console.log(e.message)
+}
+
+
+
+/*// Importación de la clase App
+const App = require('./src/App')
+
+// Instancia de la clase App
+const app = new App
+
+try {
     const result = app.registrar(
         'cosme@fulanito.fox',   // email
         'secretoo'             // password
@@ -14,3 +35,4 @@ try {
 } catch (e) {
     console.log(e.message)
 }
+*/
