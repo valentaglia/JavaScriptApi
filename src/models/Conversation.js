24944f4691f845530d3a5b651a056ab9daaf6c76
Joel <valenciajoel0@gmail.com>
2023-05-23 20:22:15 -0300

    Creacion de clase Convesation

--- src/models/Conversation.js
+++ src/models/Conversation.js
@@ -1,20 +1,29 @@
+const Factory = require('../factories/UserFactory')
 class Conversation{
-    #initiator
-    #receiver
+    
+    #initiator 
+    #receiver 
+    //
     constructor(initiator, receiver){
-        this.#receiver = receiver
+        this.#receiver = this.toContact(receiver)
         this.#initiator = initiator
     }
 
     sendMessage(){
+        console.log(this.#initiator.getId())
         let message = {
             initiator_id: this.#initiator.getId(),
             receiver_id: this.#receiver.getId(),
             blocked : false,
             blocked_by : null,
         }
-        console.log(message);
+        let content = JSON.stringify(message)
+        console.log(content);
 
+    }
+    // devuelve el objeto user del contacto
+    toContact(receiver){
+        return Factory.getContact(receiver)
     }
 }
 
