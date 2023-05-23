8c9329da48b9eed61794e1fe5267841e0ba29647
Javier Iván Moreno <Rannmoreno@gmail.com>
2023-05-23 16:42:01 -0300

    Factory de eventos

---
+++ src/factories/EventFactory.js
@@ -0,0 +1,13 @@
+const Event = require('../models/Events')
+
+class EventFactory {
+    static make(obj) {
+        let event = new Event
+        
+        
+
+        return event
+    }
+}
+
+module.exports = EventFactory
