74baa8f3cf55fbdd78bd25da86a556cc6e64ccdf
Joel <valenciajoel0@gmail.com>
2023-05-23 20:54:21 -0300

    Se implemento Extends de Repository

--- src/repositories/ContactsRepository.js
+++ src/repositories/ContactsRepository.js
@@ -1,29 +1,12 @@
 const fs = require("fs");
-class ContactRepository {
-  file = "D:/ORT/2do año Primer Cuatrimestre/TP2/ProyectoTP2/ORT-2023-1/src/repositories/contacts.json";
-
-  read() {
-    let content = fs.readFileSync(this.file);
-    return JSON.parse(content);
-  }
+const Repository = require("./Repository");
+class ContactRepository extends Repository{
+  file = "./contacts.json";
 
   findByEmail(email) {
     let contacts = this.read();
     return contacts.find((contact) => contact.email === email);
   }
 
-  save(data) {
-    let content = this.read()
-
-    data.id = Date.now()
-
-    content.push(data)
-
-    content = JSON.stringify(content)
-
-    fs.writeFileSync(this.file, content)
-
-    return true;
-}
 }
 module.exports = ContactRepository;
