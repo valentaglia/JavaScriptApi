355971ffaa16962680bdaca6865d668d38b9a60f
Santiago <santykantor@gmail.com>
2023-05-23 20:32:48 -0300

    fix: Se agrego la validacion de caracteres

--- src/validators/categoryValidator.js
+++ src/validators/categoryValidator.js
@@ -4,5 +4,16 @@ module.exports = {
         if (typeof name !== "string" && name != "") {
             throw new Error('Se esperaba un nombre en string')
         }
+
+        const nameMinLength = 1
+        const nameMaxLength = 100
+
+        if(name.length < nameMinLength){
+            throw new Error (`El nombre tiene que tener como minimo ${nameMinLength} caracteres`)
+        }
+
+        if(name.length > nameMaxLength){
+            throw new Error (`El nombre tiene que tener como maximo ${nameMaxLength} caracteres`)
+        }
     }
 }
