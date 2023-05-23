9356148bad017550dc8d8c828d8fed28f8d237c0
danymor83 <danymor83@gmail.com>
2023-05-23 20:52:44 -0300

    creacion de createMeeting

--- index.js
+++ index.js
@@ -14,3 +14,19 @@ try {
 } catch (e) {
     console.log(e.message)
 }
+
+// Intancia de la clase App
+try{
+
+    const calendar = new App
+
+    const meeting = calendar.createMeeting({
+        "date" : "2023-01",
+        "time" : "17:30",
+        "duration" : 30,
+        })
+
+}catch(e){
+    console.log(e.message)
+}
+
