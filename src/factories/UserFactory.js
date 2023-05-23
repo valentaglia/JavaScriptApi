84e3b63ce7ee3f6ac1c08d42e9a731ca48974f9d
Joel <valenciajoel0@gmail.com>
2023-05-23 20:19:42 -0300

    Implemente un metodo que devuelva un Contact dentro de los user

--- src/factories/UserFactory.js
+++ src/factories/UserFactory.js
@@ -4,10 +4,18 @@ class UserFactory {
     static make(obj) {
         let user = new User
         
-        user.setId(obj.id)
+        user.setId(Date.now())
         user.setEmail(obj.email)
         user.setPassword(obj.password)
+        user.setContacts()
 
+        return user
+    }
+    //Metodo para obtener el objeto userContact
+    static getContact(obj){
+        let user = new User
+        user.setId(obj.id)
+        user.setEmail(obj.email)
         return user
     }
 }
