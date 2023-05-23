a291701ff8905ce7e6e55b08f5e14d83dc515bcc
Agustin Kryss <aguskryss@gmail.com>
2023-05-23 19:32:09 -0300

    Agrego invitar-reunion.js

--- invitar-reunion.js
+++ invitar-reunion.js
@@ -9,18 +9,6 @@ const calendar = new Calendar();
 const users = new User();
 
 
-// Agregar reuniones al calendario
-// const meeting1 = { id: 1, startTime: '2023-05-16 08:30:00', endTime: '2023-05-16 09:00:00', duration: 30 };
-// const meeting2 = { id: 2, startTime: '2023-05-16 08:30:00', endTime: '2023-05-16 09:00:00',  duration: 30 };
-
-// calendar.addMeeting(1, meeting1);
-
-
-// calendar.addMeeting(2, meeting2);
-
-// console.log(calendar);
-
-
 // Reunion a la que voy a sumar invitados
 const meeting = calendar.getMeetingById(1)
 // Busco por email a la persona que debo invitar
@@ -28,10 +16,6 @@ const guest = users.find('email@example.com')
 
 
 
-
-// // Lo agrego a la reunion
-
-// const guest1 = {id: 1, email: "aguskryss@gmail.com"}
 
 const meetingInstance = new Meeting();
 
