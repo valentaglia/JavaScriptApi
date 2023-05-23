7aabd258244720a22418ac05134029af538fd333
Joel <valenciajoel0@gmail.com>
2023-05-23 20:16:56 -0300

    Implementacion de repositorio de Contacto para manipular Contacto

--- src/repositories/ContactsRepository.js
+++ src/repositories/ContactsRepository.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 class ContactRepository {
-  file = "./contacts.json";
+  file = "D:/ORT/2do año Primer Cuatrimestre/TP2/ProyectoTP2/ORT-2023-1/src/repositories/contacts.json";
 
   read() {
     let content = fs.readFileSync(this.file);
@@ -11,5 +11,19 @@ class ContactRepository {
     let contacts = this.read();
     return contacts.find((contact) => contact.email === email);
   }
+
+  save(data) {
+    let content = this.read()
+
+    data.id = Date.now()
+
+    content.push(data)
+
+    content = JSON.stringify(content)
+
+    fs.writeFileSync(this.file, content)
+
+    return true;
+}
 }
 module.exports = ContactRepository;
