e92c20cca5870d33dd78cde449c4e5499b8a2f29
SantiagoOlivera <olivera.santiagoariel@gmail.com>
2023-05-27 13:20:47 -0300

    Test de llamada a las funciones resize de imagenes y validaciones

--- testMultimedia.js
+++ testMultimedia.js
@@ -2,16 +2,35 @@
 const Multimedia = require('./src/Multimedia');
 
 // Instancia de la clase App
-const app = new Multimedia
+const multi = new Multimedia
 
 try {
 
-    app.resizeImage(
+    multi.resizeImage(
         './src/assets/img/DarthVader.jpg',
         '50x50'
-    )
+    );
 
-    app.resizeImage(
+    //test validacion archivo no existente
+    multi.resizeImage(
+        './src/assets/img/DarthVader1.jpg',
+        '50x50'
+    );
+    
+    //test validacion ancho en 0
+    multi.resizeImage(
+        './src/assets/img/DarthVader.jpg',
+        '0x50'
+    );
+
+    //test validacion alto en 0
+    multi.resizeImage(
+        './src/assets/img/DarthVader.jpg',
+        '50x0'
+    );
+
+
+    multi.resizeImage(
         './src/assets/img/ArgentinaCampeon2022.jpg',
         '150x120'
     )
