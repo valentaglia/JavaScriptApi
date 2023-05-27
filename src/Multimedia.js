4fb87c77ab3916c25cd496b31763d9051f09dc67
SantiagoOlivera <olivera.santiagoariel@gmail.com>
2023-05-27 13:10:06 -0300

    Se adapto el codigo para no usar Factory y Repository. Ahora usa MultimediaStorage para llamar a las funcion de resize de imagees

--- src/Multimedia.js
+++ src/Multimedia.js
@@ -1,48 +1,53 @@
 const { validateResizeImage } = require('./validators/MultimediaValidator');
-const MultimediaFactory = require('./factories/MultimediaFactory');
-const MultimediaRepository = require('./repositories/MultimediaRepository');
+const MultimediaStorage = require('./storage/MultimediaStorage');
+const DEFAULT_PATH_RESIZED_IMAGES = './src/assets/resized-images/';
+const EXT_SEP = '.';
 
 class Multimedia {
     
     async resizeImage(path, size) {
+        try{
+            const SEP = 'x';
 
-        const SEP = 'x';
+            await validateResizeImage(path, size);
 
-        validateResizeImage(path, size);
-
-        var rep = new MultimediaRepository();
-        var img = await MultimediaFactory.getImage(path);
+            var sto = new MultimediaStorage();
+            var img = await sto.getImage(path);
         
-        console.log(
-            "Imagen", 
-            img, 
-            img.getPath(), 
-            img.getFilename(),
-            img.getExt(),
-            img.getWidth(), 
-            img.getHeight()
-        );
+            console.log(
+                "Imagen", 
+                img, 
+                img.getPath(), 
+                img.getFilename(),
+                img.getExt(),
+                img.getWidth(), 
+                img.getHeight()
+            );
 
-        var split = size.split(SEP);
-        var w = Number(split[0]);
-        var h = Number(split[1]);
+            var split = size.split(SEP);
 
-        var resizedImg = await MultimediaFactory.getResizedImage(img, w, h);
+            var w = Number(split[0]);
+            var h = Number(split[1]);
 
-        console.log(
-            "Imagen resized", 
-            resizedImg, 
-            resizedImg.getPath(), 
-            resizedImg.getFilename(),
-            resizedImg.getExt(),
-            resizedImg.getWidth(), 
-            resizedImg.getHeight()
-        );
+            var resizedImg = await sto.resizeImage(img, w, h);
 
-        await rep.setImage(`./src/assets/resized-images/${resizedImg.getFilename()}${w}${SEP}${h}.${resizedImg.getExt()}`, resizedImg);
+            resizedImg.setPath(`${DEFAULT_PATH_RESIZED_IMAGES}${resizedImg.getFilename()}${w}${SEP}${h}${EXT_SEP}${resizedImg.getExt()}`);
 
-        return resizedImg;
-
+            console.log(
+                "Imagen resized", 
+                resizedImg, 
+                resizedImg.getPath(), 
+                resizedImg.getFilename(),
+                resizedImg.getExt(),
+                resizedImg.getWidth(), 
+                resizedImg.getHeight()
+            );
+        
+            await sto.setImage(resizedImg);
+            
+        }catch(ex){
+            console.log(ex);
+        }
     }
 
 }
