3f2f458cb42e66ee301fb23fbc95c3066681a748
SantiagoOlivera <olivera.santiagoariel@gmail.com>
2023-05-27 13:19:45 -0300

    Se agregaron validaciones para validar que exista el archivo en la ruta indicada para el resize de imagenes y validar que pase un ancho y alto mayor a 0

--- src/validators/MultimediaValidator.js
+++ src/validators/MultimediaValidator.js
@@ -1,14 +1,32 @@
+const SEP = 'x';
+const MSG_ERROR_INVALID_SIZE = 'Tamaño imagen ingresado invalido';
+const MSG_ERROR_INVALID_SIZE_WIDTH_GREATER_ZERO = 'El ancho debe ser mayor a 0';
+const MSG_ERROR_INVALID_SIZE_HEIGHT_GREATER_ZERO = 'El alto debe ser mayor a 0';
+const MSG_ERROR_INVALID_PATH_FILE = 'No existe el archivo en: ';
+const fs = require('fs');
+
 module.exports = {
-    validateResizeImage(path, size){
-        const MSG_ERROR_INVALID_SIZE = 'Size ingresado invalido';
-        const SEP = 'x';
+    async validateResizeImage(path, size) {
+
         var split = size.split(SEP);
-        if(split.length === 2){
-            if(isNaN(split[0])){
+        var exists = await fs.existsSync(path);
+        
+        if(!exists) {
+            throw(MSG_ERROR_INVALID_PATH_FILE + path);
+        }
+        
+        if(split.length === 2) {
+
+            if(isNaN(split[0])) {
                 throw(MSG_ERROR_INVALID_SIZE);
-            }else if(isNaN(split[1])){
+            }else if(Number(split[0]) <= 0){
+                throw(MSG_ERROR_INVALID_SIZE_WIDTH_GREATER_ZERO);
+            } else if(isNaN(split[1])){
                 throw(MSG_ERROR_INVALID_SIZE);    
+            }else if(Number(split[1]) <= 0) {
+                throw(MSG_ERROR_INVALID_SIZE_HEIGHT_GREATER_ZERO);
             }
+            
         }else{
             throw(MSG_ERROR_INVALID_SIZE);
         }
