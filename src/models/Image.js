9b9a5bf8c8dc1ad0450b8a76d77dea2a47a87a8f
SantiagoOlivera <olivera.santiagoariel@gmail.com>
2023-05-27 13:16:03 -0300

    Se quito codigo inecesario

--- src/models/Image.js
+++ src/models/Image.js
@@ -1,24 +1,9 @@
-const MultimediaFactory = require('../factories/MultimediaFactory');
-const MultimediaRepository = require('../repositories/MultimediaRepository');
 const File = require('../models/File');
 
 class Image extends File {
 
     #width
     #height
-
-    /* async resize(width, height) {
-        
-        var rep =  new MultimediaRepository();
-
-        var buffer = await rep.resizeImage(
-            this.getPath(), 
-            width, 
-            height
-        );
-
-        this.setBuffer(buffer);
-    } */
 
     setWidth(w){
         this.#width = w;
