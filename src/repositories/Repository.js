7bf99a10c66cd51b845b5ae36cd23a2dbd78cf04
Jplevet <juann.levet@hotmail.com>
2023-05-28 21:11:54 -0300

    Se elimina codigo de la funcion convertirImgAjpg que estaba demas en este archivo

--- src/repositories/Repository.js
+++ src/repositories/Repository.js
@@ -27,32 +27,7 @@ class Repository {
         fs.writeFileSync(this.file, content)
 
         return true;
-    }
-
-    //CONVERTIR IMG A JPG
-
-    convertirImgAjpg(archivo){
-        const rutaImgPNG = './image.png' // en clase hija especificar la ruta y dejar la variable sin especificar aca
-        const rutaImgJpg = './image.jpg' // lo mismo q arriba
-        
-        try {
-            // Accedo a la imagen que voy a convertir
-            const image = fs.readFileSync(rutaImgPNG)            
-        
-            sharp(image).toJpeg().toFile(rutaImgJpg, (error, info) => {
-                if (error) {
-                    console.log('Error al convertir la imagen', error)
-                    return
-                }
-                console.log('Resultado exitoso', info)    
-            })            
-        } catch (error) {
-            console.log('Error al leer la imagen', error)
-        }
-
-       /*  // Este método copia la imagen con el mismo nombre y misma carpeta a .jpeg
-        const copy = image.toJpeg() */
-    }    
+    }   
 
 }
 
