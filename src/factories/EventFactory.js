6db2ef375d8eeaae78b5b3005f9d8511760dc40c
Javier Iván Moreno <Rannmoreno@gmail.com>
2023-05-30 15:32:29 -0300

    Actualizacion de EventFactory.js

--- src/factories/EventFactory.js
+++ src/factories/EventFactory.js
@@ -1,10 +1,11 @@
-const Event = require('../models/Events')
-
+const Event = require('../models/Event')
 class EventFactory {
     static make(obj) {
         let event = new Event
         
-        
+        event.setDateEvent(obj.dateEvent)
+        event.setStartEvent(obj.startEvent)
+        event.setEndEvent(obj.endEvent)
 
         return event
     }
