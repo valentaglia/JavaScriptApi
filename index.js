7de6240284578198184d22a5e9ba637273b25f77
danymor83 <danymor83@gmail.com>
2023-06-01 20:35:31 -0300

    actualizacion index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const App = require('./src/App')
 
 // Instancia de la clase App
 const app = new App
-
+/*
 try {
     const result = app.registrar(
         'cosme@fulanito.fox',   // email
@@ -13,18 +13,18 @@ try {
     console.log(result)
 } catch (e) {
     console.log(e.message)
-}
+}*/
 
 // Intancia de la clase App
 try{
 
     const calendar = new App
 
-    const meeting = calendar.createMeeting({
-        "date" : "2023-01",
-        "time" : "17:30",
-        "duration" : 30,
-        })
+    const meeting = calendar.createMeeting(
+        "2023-06-03",
+        "17:30",
+         30,
+        )
 
 }catch(e){
     console.log(e.message)
