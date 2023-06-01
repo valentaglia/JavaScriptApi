fdb009e59b8f6319f930b7c67d9090a11d38a0e3
danymor83 <danymor83@gmail.com>
2023-06-01 20:31:47 -0300

    actualizacion MeetRepository

--- src/repositories/MeetRepository.js
+++ src/repositories/MeetRepository.js
@@ -1,8 +1,23 @@
 const Meet = require('../models/Meet')
+const Factory = require('../factories/MeetFactory')
 const Repository = require('./Repository')
 
 class MeetRepository extends Repository {
     file = './meets.json'
+
+    byId(id) {
+        if (isNaN(id)) {
+            throw new Error('Se esperaba id numerico')
+        }
+
+        let meet = this.findById(id)
+
+        if (meet === undefined) {
+            throw new Error('El Meet no se encontro')
+        }
+
+        return Factory.make(meet)
+    }
 
     create(meet) {
         if (meet instanceof Meet) {
@@ -17,5 +32,6 @@ class MeetRepository extends Repository {
 
         throw new Error('Se requiere una instancia de Meet')
     }
-
 }
+
+module.exports = MeetRepository
