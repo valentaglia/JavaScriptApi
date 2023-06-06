1e081a19066efb8d06203155b9c89f2e92fd82f9
mr_pensa <mariopensa97@gmail.com>
2023-06-06 19:44:42 -0300

    Codigo de nodemailer

---
+++ src/repositories/pushEmail.js
@@ -0,0 +1,44 @@
+//Importe de libreria
+const nodemailer = require('nodemailer');
+const passwordAplication = require('../validators/passwordNodemailer')
+const dotenv = require('dotenv').config();
+
+
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        host: 'smtp.gmail.com',
+        port: 587,
+        auth: {
+            user: 'testort2023@gmail.com',
+            pass: process.env.PASSWORD
+        }
+      });
+
+
+      const mailOptions = {
+        from:"testort2023@gmail.com", 
+        to:"testort2023@gmail.com", 
+        subject: 'Asunto del correo',
+        text: 'Correo enviado con la password guardada en otra clase'
+      };
+
+      transporter.sendMail(mailOptions, function(error, info) {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Correo enviado: ' + info.response);
+        }
+      });
+    
+   /* transporter.sendMail(message, (error, info) => {
+        if (error){
+            console.log(error);
+            res.send(500, err.message);
+        } else {
+            console.log("Email sent");
+            res.status(200).jsonp(req.body);
+        }
+    })
+*/
+
+
