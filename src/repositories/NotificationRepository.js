6e8ac58c4358050191ad6eaefc87d4d1df7f00b4
Nel Fernández <devneloper@hotmail.com>
2023-06-06 19:35:57 -0300

    Modificaciones en la clase NotificationRepository

--- src/repositories/NotificationRepository.js
+++ src/repositories/NotificationRepository.js
@@ -1,9 +1,27 @@
+const User = require('../models/Notification')
+const Factory = require('../factories/NotificationFactory')
+const Repository = require('./Repository');
+
 class NotificationRepository extends Repository {
 
   file = './notifications.json';
 
-  save(notification) {
-    super.save(notification);
+  create(notification) {
+    super.save(notification)
+  }
+
+  byId(id) {
+    if (isNaN(id)) {
+      throw new Error('Se esperaba id numérico')
+    }
+
+    let notif = this.findById(id)
+
+    if (notif === undefined) {
+        throw new Error('La notificación no se encontró')
+    }
+
+    return Factory.make(notif)
   }
 }
 
