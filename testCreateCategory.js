1ee200c47a7653c85a2836cf31180aacade01d1e
Alejandro Villafañe <info@alezvi.com.ar>
2023-06-06 15:13:27 -0300

    examples

--- testCreateCategory.js
+++
@@ -1,9 +0,0 @@
-const crearCategoria = require('./src/FunctionCategory')
-
-try{
-    let celular = {"name": "Celulares"}
-    const resultado = crearCategoria(celular)
-    console.log(resultado.getName())
-} catch(e){
-    console.log(e.message)
-}
