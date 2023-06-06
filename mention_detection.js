1be5da5e0bcfaa800f305e42370e7df3034d376d
Alejandro Villafañe <avillafane@smallcode.com.ar>
2023-06-06 15:06:06 -0300

    mention detection file

---
+++ mention_detection.js
@@ -0,0 +1,11 @@
+app.login('user@example.com', 'password');
+
+// Enviar un mensaje
+const mensaje = 'Hola @profesor tengo una consulta';
+app.sendMessage(mensaje);
+
+const mensaje1 = 'Hola @valen te puedo pedir algo?';
+app.sendMessage(mensaje1);
+
+const mensaje2 = 'Hola @director tengo un problema';
+app.sendMessage(mensaje2);
