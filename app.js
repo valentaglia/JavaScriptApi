2f82cfb6b7a77a4a926df0470b0a2918d0afb8a1
Santiago <santykantor@gmail.com>
2023-06-11 13:43:02 -0300

    feat: agrgo rutas para category

--- app.js
+++ app.js
@@ -4,12 +4,14 @@ var bodyParser = require('body-parser')
 app.use(bodyParser.json({}))
 
 const rolesRouter = require('./routes/rolesRouter')
+const categoryRouter = require('./routes/categoryRouter')
 
 app.get('/', function (req, res) {
     res.send('Bienvenido a la app')
 })
 
 app.use('/roles', rolesRouter)
+app.use('/roles', categoryRouter)
 // ... agregar rutas que faltan
 
 app.listen(3001, function () {
