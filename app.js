30f171025080ca539529e720635aec8459f966d6
sebmartinezpedemonte <sebastianmartinezpedemonte@gmail.com>
2023-06-11 16:42:29 -0300

    se agrego la ruta de Product

--- app.js
+++ app.js
@@ -4,13 +4,14 @@ var bodyParser = require('body-parser')
 app.use(bodyParser.json({}))
 
 const rolesRouter = require('./routes/rolesRouter')
-
+const productsRouter = require('./routes/productsRouter.js')
 app.get('/', function (req, res) {
     res.send('Bienvenido a la app')
 })
 
 app.use('/roles', rolesRouter)
 // ... agregar rutas que faltan
+app.use('/products', productsRouter)
 
 app.listen(3001, function () {
     console.log('http://localhost:3001/')
