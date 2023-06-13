870a9a697c2746c4d2ae7a09449377decfe4622b
Nel Fernández <devneloper@hotmail.com>
2023-06-13 20:24:30 -0300

    Creación del NotificationController

---
+++ src/controllers/NotificationController.js
@@ -0,0 +1,47 @@
+const NotificationRepository = require('../repositories/NotificationRepository')
+
+class NotificationController {
+    constructor() {
+        this.repo = new NotificationRepository
+    }
+
+    index = (req, res) => {
+        let notifications = this.repo.all()
+    
+        res.json(notifications)
+    }
+
+    show = (req, res) => {
+        try {
+            let notif = this.repo.findById(req.params.id)
+            res.json(notif)
+        } catch (e) {
+            res.json({error: e.message})
+        }
+    }
+
+    create = (req, res) => {
+        // valido
+        if (req.body.text.length === 0) {
+            return res.status(422).end('TEXT_IS_EMPTY')
+        }
+
+        if (req.body.text.length > 144) {
+            return res.status(422).end('TEXT_TOO_LONG')
+        }
+
+        // guardo
+        this.repo.create({
+            text : req.body.text
+        })
+
+        // respondo
+        res.status(201).json({})
+    }
+    
+    update = (req, res) => {}
+    
+    delete = (req, res) => {}
+}
+
+module.exports = NotificationController
