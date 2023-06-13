52ace853f61e8fbffb4a94e0260795fa4d57d3e2
mr_pensa <mariopensa97@gmail.com>
2023-06-13 20:22:13 -0300

    Clase que tiene los datos del mail de salida

---
+++ src/models/DataMail.js
@@ -0,0 +1,16 @@
+class DataMail {
+    constructor() {
+      this.pass = "gofgkhgfswjsaaxi";
+      this.mailSalida = 'testort2023@gmail.com'
+    }
+  
+    getPass() {
+      return this.pass;
+    }
+  
+    getSalida() {
+      return this.mailSalida;
+    }
+  }
+  
+  module.exports = DataMail;
