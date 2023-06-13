97a5f75a0d9b0b6e5c0d9d688b473fe8168536b6
valentaglia <vaalen.tagliaferro@hotmail.com>
2023-06-13 19:43:53 -0300

    Correccion de errores

--- src/controllers/MentionsController.js
+++ src/controllers/MentionsController.js
@@ -1,13 +1,14 @@
 const MentionExtractor = require('../utils/extraerMenciones');
 
 class MentionsController {
-    constructor() {
-        this.repo = new MentionsController
-    } 
+  constructor() {
+    this.extractor = new MentionExtractor();
+  } 
+
   extractMentions(req, res) {
     try {
       const message = req.body.message;
-      const mentions = MentionExtractor.extract(message);
+      const mentions = this.extractor.extract(message);
       res.json(mentions);
     } catch (error) {
       res.status(500).json({ error: error.message });
