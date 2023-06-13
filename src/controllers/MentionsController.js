3e7c66c9005d24e1d90c7709470c1ecd4673633e
valentaglia <vaalen.tagliaferro@hotmail.com>
2023-06-13 20:08:12 -0300

    Completando MentionsController

--- src/controllers/MentionsController.js
+++ src/controllers/MentionsController.js
@@ -1,9 +1,12 @@
+const MentionRepository = require('../repositories/MentionRepository');
 const MentionExtractor = require('../utils/extraerMenciones');
 
 class MentionsController {
   constructor() {
+    this.repo = new MentionRepository();
     this.extractor = new MentionExtractor();
-  } 
+
+  }
 
   extractMentions(req, res) {
     try {
@@ -14,6 +17,66 @@ class MentionsController {
       res.status(500).json({ error: error.message });
     }
   }
+
+  index(req, res) {
+    try {
+      const mentions = this.repo.all();
+      res.json(mentions);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  }
+
+  create(req, res) {
+    try {
+      // Validaciones de los datos recibidos
+      // ...
+      
+      // Guardar la mención en el repositorio
+      const mention = this.repo.save(req.body);
+      
+      // Responder con el resultado
+      res.status(201).json(mention);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  }
+
+  update(req, res) {
+    try {
+      // Validaciones de los datos recibidos
+      // ...
+      
+      // Obtener el ID de la mención a actualizar
+      const { id } = req.params;
+      
+      // Actualizar la mención en el repositorio
+      const mention = this.repo.update(id, req.body);
+      
+      // Responder con el resultado
+      res.json(mention);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  }
+
+  delete(req, res) {
+    try {
+      // Obtener el ID de la mención a eliminar
+      const { id } = req.params;
+      
+      // Eliminar la mención del repositorio
+      this.repo.deleteById(id);
+      
+      // Responder con éxito
+      res.status(204).end();
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  }
+
+
+  
 }
 
 module.exports = MentionsController;
---
+++ src/repositories/MentionRepository.js
@@ -0,0 +1,37 @@
+class MentionRepository {
+    constructor() {
+      // Array para mantener un registro de las menciones
+      this.mentions = [];
+    }
+  
+    // Obtener todas las menciones
+    all() {
+      return this.mentions;
+    }
+  
+    // Guardar una nueva mención
+    save(mention) {
+      this.mentions.push(mention);
+    }
+  
+    // Buscar una mención por su ID
+    findById(id) {
+      const mention = this.mentions.find((mention) => mention.id === id);
+      if (!mention) {
+        throw new Error('Mention not found');
+      }
+      return mention;
+    }
+  
+    // Eliminar una mención por su ID
+    deleteById(id) {
+      const index = this.mentions.findIndex((mention) => mention.id === id);
+      if (index === -1) {
+        throw new Error('Mention not found');
+      }
+      this.mentions.splice(index, 1);
+    }
+  }
+  
+  module.exports = MentionRepository;
+  
