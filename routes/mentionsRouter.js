90a9929ab07da01f49b68b379c3dc8ec20c75168
valentaglia <vaalen.tagliaferro@hotmail.com>
2023-06-13 20:09:20 -0300

    Agrego rutas al Router

--- routes/mentionsRouter.js
+++ routes/mentionsRouter.js
@@ -4,5 +4,15 @@ const router = express.Router();
 const controller = new MentionsController();
 
 router.post('/extract', controller.extractMentions);
+// Ruta para obtener todas las menciones
+router.get('/', controller.index);
 
+// Ruta para crear una nueva mención
+router.post('/', controller.create);
+
+// Ruta para actualizar una mención por su ID
+router.put('/:id', controller.update);
+
+// Ruta para eliminar una mención por su ID
+router.delete('/:id', controller.delete);
 module.exports = router;
