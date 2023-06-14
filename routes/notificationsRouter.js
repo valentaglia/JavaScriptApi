786429e46e501e5a543e2342e47130a9e420f545
Nel Fernández <devneloper@hotmail.com>
2023-06-13 23:43:07 -0300

    Depuración

--- routes/notificationsRouter.js
+++ routes/notificationsRouter.js
@@ -5,8 +5,8 @@ const controller = new NotificationController
 // http://localhost:3001/notifications
 router.get('/', controller.index) // /notifications
 router.get('/:id', controller.show) // /notifications/:id
-router.post('/', controller.create) // /notifications
-router.put('/:id', controller.update) // /notifications/:id
+router.post('/', controller.create) // /notification
+router.put('/:id', controller.update) // /notification/:id
 router.delete('/:id', controller.delete) // /notifications/:id
 
 module.exports = router
