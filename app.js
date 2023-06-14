b11a3355c6184e7fe9ba0d77215a3954089516ba
Agustin Kryss <aguskryss@gmail.com>
2023-06-13 22:34:26 -0300

    Ruteo en el app

--- app.js
+++ app.js
@@ -4,8 +4,7 @@ var bodyParser = require('body-parser')
 app.use(bodyParser.json({}))
 
 const rolesRouter = require('./routes/rolesRouter')
-const storeRouter = require('./routes/storeRouter')
-const cartRouter = require('./routes/cartRouter')
+const meetRouter = require('./routes/meetRouter')
 
 app.get('/', function (req, res) {
     res.send('Bienvenido a la app')
@@ -13,8 +12,8 @@ app.get('/', function (req, res) {
 
 app.use('/roles', rolesRouter)
 // ... agregar rutas que faltan
-app.use('/stores',storeRouter)
-app.use('/carts',cartRouter)
+
+app.use('/meet', meetRouter)
 
 app.listen(3001, function () {
     console.log('http://localhost:3001/')
