75ad699613e8dc5ce24d8c77fda3aee2583b6c7b
termoit <termoit@gmail.com>
2023-06-13 23:28:28 -0300

    Se agrega un Middleware para validar datos antes del metodo create

--- routes/metadataRouter.js
+++ routes/metadataRouter.js
@@ -1,12 +1,13 @@
 const router = require('express').Router()
 const MetadataController = require('../src/controllers/MetadataController')
 const controller = new MetadataController
+const validator = require('../src/middlewares/metadataValidationMiddleware')
 
-// http://localhost:3001/roles
-router.get('/', controller.index) // /roles
-router.get('/:id', controller.show) // /roles/:id
-router.post('/', controller.create) // /roles
-router.put('/:id', controller.update) // /roles/:id
-router.delete('/:id', controller.delete) // /roles/:id
+// http://localhost:3001/metadata
+router.get('/', controller.index) // /metadata
+router.get('/:id', controller.show) // /metadata/:id
+router.post('/', validator, controller.create) // /metadata
+router.put('/:id',controller.update) // /metadata/:id
+router.delete('/:id', controller.delete) // /metadata/:id
 
 module.exports = router
