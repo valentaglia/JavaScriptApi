eabf2345f9aa6717188e2849af2a123b3b04beed
valentaglia <vaalen.tagliaferro@hotmail.com>
2023-06-13 22:16:37 -0300

    Validacion en lugar correcto

--- routes/mentionsRouter.js
+++ routes/mentionsRouter.js
@@ -1,8 +1,19 @@
 const router = require('express').Router()
 
 const MentionsController = require('../src/controllers/MentionsController');
+const validator = require('../src/validators/mentionValidator')
 
 const controller = new MentionsController();
+function middleware(req, res, next) {
+    // Validar los datos recibidos
+    const validationResult = validator(req.body);
+
+    if (!validationResult.isValid) {
+        // Si la validación falla, responder con un código de estado 400 y los errores de validación
+        return res.status(400).json({ errors: validationResult.errors });
+    }
+}
+
 
 router.post('/extract', controller.extractMentions);
 // Ruta para obtener todas las menciones
@@ -12,8 +23,8 @@ router.get('/', controller.index);
 router.post('/', middleware, controller.create);
 
 // Ruta para actualizar una mención por su ID
-router.put('/:id',middleware, controller.update);
-
+router.put('/:id', middleware, controller.update);
+//funcion que recibe la petición antes que el metodo final del controlador
 // Ruta para eliminar una mención por su ID
 router.delete('/:id', controller.delete);
 module.exports = router;
--- src/controllers/MentionsController.js
+++ src/controllers/MentionsController.js
@@ -1,6 +1,5 @@
 const MentionRepository = require('../repositories/MentionRepository');
 const extraerMenciones = require('../utils/extraerMenciones');
-const validator = require('../validators/mentionValidator')
 
 
 class MentionsController {
@@ -27,14 +26,6 @@ class MentionsController {
     }
   };
   create = (req, res) => {
-    // Validar los datos recibidos
-    const validationResult = validator(req.body);
-  
-    if (!validationResult.isValid) {
-      // Si la validación falla, responder con un código de estado 400 y los errores de validación
-      return res.status(400).json({ errors: validationResult.errors });
-    }
-  
     // Guardar la mención en el repositorio
     const mention = this.repo.save(req.body);
     // Responder con el resultado
@@ -42,19 +33,10 @@ class MentionsController {
   };
 
   update = (req, res) => {
-    // Validar los datos recibidos
-    const validationResult = validator(req.body);
-  
-    if (!validationResult.isValid) {
-      // Si la validación falla, responder con un código de estado 400 y los errores de validación
-      return res.status(400).json({ errors: validationResult.errors });
-    }
-  
     // Obtener el ID de la mención a actualizar
     const { id } = req.params;
     // Actualizar la mención en el repositorio
     const mention = this.repo.update(id, req.body);
-  
     // Responder con el resultado
     res.json(mention);
   };
