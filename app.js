a4ffcca0475ab8dc8ed92cc4e7f61700134268ee
Nel Fernández <devneloper@hotmail.com>
2023-06-13 23:44:55 -0300

    Agregué la ruta del controlador de notificaciones

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ app.use(bodyParser.json({}))
 const rolesRouter = require('./routes/rolesRouter')
 const storeRouter = require('./routes/storeRouter')
 const cartRouter = require('./routes/cartRouter')
+const notificationsRouter = require('./routes/notificationsRouter')
 
 app.get('/', function (req, res) {
     res.send('Bienvenido a la app')
@@ -15,6 +16,7 @@ app.use('/roles', rolesRouter)
 // ... agregar rutas que faltan
 app.use('/stores',storeRouter)
 app.use('/carts',cartRouter)
+app.use('/notifications', notificationsRouter)
 
 app.listen(3001, function () {
     console.log('http://localhost:3001/')
