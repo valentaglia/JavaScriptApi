08ef05377d50f85644d5b88e22067bd27a61bb55
FrancoMarini82 <>
2023-06-14 08:23:25 -0300

    FIX se agrega un middleware en post

--- routes/storeRouter.js
+++ routes/storeRouter.js
@@ -1,12 +1,12 @@
-const router = require ('express').Router()
+const router = require('express').Router()
 const StoreController = require('../src/controllers/StoreController')
 const controller = new StoreController()
+const validator = require('../src/middlewares/storeValidationMiddelware')
 
+router.get('/', controller.index)
+router.get('/:id', controller.show)
+router.post('/', validator.validate, controller.create)
+router.put('/:id', controller.update)
+router.delete('/:id', controller.delete)
 
-router.get ('/', controller.index)
-router.get ('/:id', controller.show)
-router.post ('/', controller.create)
-router.put ('/:id', controller.update)
-router.delete ('/:id', controller.delete)
-
-module.exports= router
+module.exports = router
