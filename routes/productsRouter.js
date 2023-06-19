8e04b56877faf9df53069e401be5afccb80709ca
sebmartinezpedemonte <sebastianmartinezpedemonte@gmail.com>
2023-06-19 19:38:48 -0300

    se agrego el middleware de validacion al router

--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -1,12 +1,14 @@
 const router = require('express').Router()
 const ProductsController = require('../src/controllers/ProductsController')
+const validation = require('../src/middlewares/productsValidatorMiddleware')
 const controller = new ProductsController
+
 
 // http://localhost:3001/products
 router.get('/', controller.index) // /products
 router.get('/:id',controller.show) // /products/:id
-router.post('/', controller.create) // /products
-router.put('/:id', controller.update) // /products/:id
+router.post('/', validation, controller.create) // /products
+router.put('/:id', validation, controller.update) // /products/:id
 router.delete('/:id', controller.delete) // /products/:id
 
 module.exports = router
