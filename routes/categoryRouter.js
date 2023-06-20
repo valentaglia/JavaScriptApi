cce22fe27d06e264313c59d7ffc8d11d8968aaf0
Santiago <santykantor@gmail.com>
2023-06-20 17:51:28 -0300

    fix: se cambio el post para category

--- routes/categoryRouter.js
+++ routes/categoryRouter.js
@@ -1,10 +1,11 @@
 const router = require('express').Router()
 const CategoryController = require('../src/controllers/CategoryController')
 const controller = new CategoryController
+const validator = require('../src/middlewares/categoryValidatorMiddleware')
 
 router.get('/', controller.index)
 router.get('/:id', controller.show)
-router.post('/', controller.create) 
+router.post('/', validator, controller.create) 
 router.put('/:id', controller.update) 
 router.delete('/:id', controller.delete) 
 
