68e511f97fabadf9ac73bb0bacda160030fdfb62
danymor83 <danymor83@gmail.com>
2023-06-25 13:09:06 -0300

    se agrego meetRouter

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ app.use(bodyParser.json({}))
 const rolesRouter = require('./routes/rolesRouter')
 const storeRouter = require('./routes/storeRouter')
 const cartRouter = require('./routes/cartRouter')
+const meetRouter = require('./routes/meetRouter')
 
 app.get('/', function (req, res) {
     res.send('Bienvenido a la app')
@@ -15,6 +16,7 @@ app.use('/roles', rolesRouter)
 // ... agregar rutas que faltan
 app.use('/stores',storeRouter)
 app.use('/carts',cartRouter)
+app.use('/meets',meetRouter)
 
 app.listen(3001, function () {
     console.log('http://localhost:3001/')
