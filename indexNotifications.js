4636c215231ea4c9433bde24a12f763e185be4d7
Nelo <devneloper@hotmail.com>
2023-06-26 19:22:54 -0300

    Actualización en indexNotifications.js

--- indexNotifications.js
+++ indexNotifications.js
@@ -1,20 +1,31 @@
 // Importación de la clase App
 //const App = require('./src/App')
-const AppNotification = require('./src/AppNotification')
+const AppNotification = require('./src/AppNotification');
+const NotificationRepository = require('./src/repositories/NotificationRepository');
 
 // Instancia de la clase App
 //const app = new App
-const app = new AppNotification
+const app = new AppNotification;
+const repo = new NotificationRepository;
 
 try { 
     const result = app.saveNotification({
-        key: "lorem ipsum test",
-        text: "Lorem ipsum dolor situm amet",
-        sendDate: "2023-06-12 19:45:35",
-        readDate: null
+        key: "Test 1",
+        text: "Test 1",
+        sendDate: "Test 1",
+        readDate: "Test 1"
     })
-
-    console.log(result)
+    console.log(result);
 } catch (e) {
-    console.log(e.message)
+    console.log(e.message);
 }
+
+try {
+    console.log("START!")
+    const result = repo.readNotification("123");
+    console.log("pasó el readNotification")
+    console.log(result);
+} catch (e) {
+    console.log(e.message);
+}
+
