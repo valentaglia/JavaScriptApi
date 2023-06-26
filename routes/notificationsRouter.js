7e7c5c06aff2208723ba309980a8138e96c978ad
Nelo <devneloper@hotmail.com>
2023-06-26 19:23:45 -0300

    Se añadieron rutas para distintas acciones

--- routes/notificationsRouter.js
+++ routes/notificationsRouter.js
@@ -5,8 +5,10 @@ const controller = new NotificationController
 // http://localhost:3001/notifications
 router.get('/', controller.index) // /notifications
 router.get('/:id', controller.show) // /notifications/:id
-router.post('/', controller.create) // /notification
-router.put('/:id', controller.update) // /notification/:id
+router.post('/', controller.create) // /notifications
+router.put('/:id', controller.update) // /notifications/:id
+router.put('/read/:id', controller.read) // /notifications/:id
 router.delete('/:id', controller.delete) // /notifications/:id
+router.delete('/', controller.deleteAll) // /notifications
 
 module.exports = router
