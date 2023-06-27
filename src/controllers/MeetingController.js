124ab834bf2db6a5b50c8350a2d40e82e62e2f46
veronicazarza <veronicastefaniazarza@gmail.com>
2023-06-27 18:45:47 -0300

    Se modificó el controlador con la generación del link en el update

--- src/controllers/MeetingController.js
+++ src/controllers/MeetingController.js
@@ -1,57 +1,85 @@
 const MeetingRepository = require('../repositories/MeetingRepository');
-const meetingModel = require('../models/Meeting');
+const { ShortUniqueId } = require('short-unique-id');
 
 class MeetingController {
     constructor() {
-        this.repo = new MeetingRepository
-    }
-
-    generateLink = (req, res) => {
-        try {
-            let linkMeeting = this.meetingModel.generateLink()
-            res.json(linkMeeting)
-        }catch (e) {
-                res.json({error: e.message})
-        }
+        this.repo = new MeetingRepository();
+        this.uid = new ShortUniqueId();
     }
 
     index = (req, res) => {
         try {
-            let meetings = this.repo.all()
-            res.json(meetings)
-        }catch (e) {
-            res.json({error: e.message})
-        }    
+            const meetings = this.repo.all();
+            res.json(meetings);
+        } catch (e) {
+            res.json({ error: e.message });
+        }
     }
 
     show = (req, res) => {
         try {
-            let meeting = this.repo.byId(req.params.id)
-            res.json(meeting)
+            const id = req.params.id;
+            const meeting = this.repo.byId(id);
+            if (!meeting) {
+                res.json({ error: 'No se encontró Meeting con el id ingresado' });
+                return;
+            }
+            res.json(meeting);
         } catch (e) {
-            res.json({error: e.message})
+            res.json({ error: e.message });
         }
     }
 
     create = (req, res) => {
         try {
-            if (req.body.date && req.body.time && req.body.duration){
-                this.repo.save({
-                    date : req.body.name,
+            if (req.body.date && req.body.time && req.body.duration) {
+                const meeting = {
+                    id: this.uid.randomUUID(10),
+                    date: req.body.date,
                     time: req.body.time,
-                    duration: req.body.duration
-                })
-            res.status(201).json({})
+                    duration: req.body.duration,
+                    link: ''
+                };
+                this.repo.save(meeting);
+                res.json(meeting);
+            } else {
+                res.json({ error: '' });
             }
         } catch (e) {
-            res.json({error: e.message})
+            res.json({ error: e.message });
         }
     }
 
-    update = (req, res) => {}
-    
-    delete = (req, res) => {}
+    update = (req, res) => {
+        try {
+            const id = req.params.id;
+            const meeting = this.repo.byId(id);
+            if (!meeting) {
+                res.json({ error: 'No se encontró Meeting con el id ingresado' });
+                return;
+            }
+            const link = this.uid.randomUUID(6);
+            meeting.link = link;
+            this.repo.save(meeting);
+            res.send(link);
+        } catch (e) {
+            res.json({ error: e.message });
+        }
+    }
+
+    delete = (req, res) => {
+        try {
+            const id = req.params.id;
+            const deleted = this.repo.delete(id);
+            if (!deleted) {
+                res.json({ error: 'No se encontró Meeting con el id ingresado' });
+                return;
+            }
+            res.send('Se eliminó la meeting exitosamente');
+        } catch (e) {
+            res.json({ error: e.message });
+        }
+    }
 }
 
-
-module.exports = MeetingController
+module.exports = MeetingController;
