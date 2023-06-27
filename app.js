1bb826c6d14e29932372d0b227ec8ab381c6602e
mr_pensa <mariopensa97@gmail.com>
2023-06-27 16:30:35 -0300

    ruta para hacer el post

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const express = require('express')
 const app = express()
 var bodyParser = require('body-parser')
 app.use(bodyParser.json({}))
+require('dotenv').config();
 
 const rolesRouter = require('./routes/rolesRouter')
 const storeRouter = require('./routes/storeRouter')
 const cartRouter = require('./routes/cartRouter')
+const emailRouter = require('./routes/emailRouter')
 
 app.get('/', function (req, res) {
     res.send('Bienvenido a la app')
@@ -15,6 +17,7 @@ app.use('/roles', rolesRouter)
 // ... agregar rutas que faltan
 app.use('/stores',storeRouter)
 app.use('/carts',cartRouter)
+app.use('/email', emailRouter)
 
 app.listen(3001, function () {
     console.log('http://localhost:3001/')
