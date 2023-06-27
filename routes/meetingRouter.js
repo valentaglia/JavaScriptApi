2de9b4d59881002cd7ff40902708ff2238fc6efa
veronicazarza <veronicastefaniazarza@gmail.com>
2023-06-27 19:21:26 -0300

    Se adapta router agregando middleware

--- routes/meetingRouter.js
+++ routes/meetingRouter.js
@@ -1,12 +1,12 @@
 const router = require('express').Router()
 const MeetingController = require('../src/controllers/MeetingController')
-
+const validation = require('../src/validators/meetingValidationMiddleware')
 const controller = new MeetingController();
 
 
 router.get('/', controller.index);
 router.get('/:id', controller.show); 
-router.post('/', controller.create);
+router.post('/',validation, controller.create);
 router.put('/:id', controller.update); 
 router.delete('/:id', controller.delete); 
 
