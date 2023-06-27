72b897be369dc84520be1cad34b432bd8fbe2b2d
mr_pensa <mariopensa97@gmail.com>
2023-06-27 16:29:18 -0300

    js para ejecutar el nodemailer

--- src/AppNodemailer.js
+++ src/AppNodemailer.js
@@ -1,13 +1,13 @@
 require('dotenv').config();
-const emailService  = require('./services/SendEmail.js');
+const EmailFactory = require('./controllers/EmailController');
 
 
 try{
 
-    const email = new emailService();
+    const email = new EmailFactory();
 
 //Recibe tres parametros(destinatario, asunto del correo, contenido del correo electrónico. )
-    email.sendEmail('testort2023@gmail.com', 'Asunto del correo', 'Este es el contenido del correo electrónico.');
+    email.sendEmail('testort2023@gmail.com', 'Prueba final sin middleware', 'Este es el contenido del correo electrónico.');
 
 }
 catch(error){
