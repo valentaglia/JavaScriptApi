bf4e7ad3271e422f85d8ed1e3536a17024cc01b9
termoit <termoit@gmail.com>
2023-06-27 15:34:12 -0300

    Se Agregan metodos delete y update, se corrigen muchos errores

--- src/controllers/MetadataController.js
+++ src/controllers/MetadataController.js
@@ -23,11 +23,11 @@ class MetadataController {
     create = (req, res) => {
         // valido
         if (req.body.type.length === 0) {
-            return res.status(422).end('NAME_IS_EMPTY')
+            return res.status(422).end('TYPE_IS_EMPTY')
         }
 
         if (req.body.type.length > 10) {
-            return res.status(422).end('NAME_TOO_LONG')
+            return res.status(422).end('TYPE_TOO_LONG')
         }
 
         // guardo
@@ -42,9 +42,44 @@ class MetadataController {
         res.status(201).json({})
     }
     
-    update = (req, res) => {}
+    update = (req, res) => {
+        const url = req.originalUrl // Magia para traer la URL completa desde "req"
+        const arrayStrings = url.split('/') // Magia que arma un array de todos los valores que estan entre "/"
+        const idAModificar = arrayStrings.pop()// Magia para sacar el ultimo elemento del array
+
+        const respuesta = this.repo.update({
+            id : idAModificar,
+            type : req.body.type,
+            mime : req.body.mime,
+            width : req.body.width,
+            height : req.body.height
+        })
+
+            //console.log(respuesta)
+            if (respuesta){
+                res.status(201).json({Message : "Metadata Eliminada"})
+            }
+            else {
+                res.status(404).json({Message : "Metadata no encontrada"})
+            }
+
+    }
     
-    delete = (req, res) => {}
+    delete = (req, res) => {
+        const url = req.originalUrl // Magia para traer la URL completa desde "req"
+        const arrayStrings = url.split('/') // Magia que arma un array de todos los valores que estan entre "/"
+        const idAModificar = arrayStrings.pop() // Magia para sacar el ultimo elemento del array
+        console.log(idAModificar)
+        const respuesta = this.repo.deleteById(idAModificar)
+
+        //console.log(respuesta)
+        if (respuesta){
+            res.status(201).json({Message : "Metadata Eliminada"})
+        }
+        else {
+            res.status(404).json({Message : "Metadata no encontrada"})
+        }
+    }
 }
 
 module.exports = MetadataController
